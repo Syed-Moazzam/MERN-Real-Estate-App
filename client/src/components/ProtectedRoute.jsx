43bf23126d3ec7bfe361db3e84ff177a44e80a3d
Syed-Moazzam <moazzamahmed05@gmail.com>
2024-03-18 19:33:03 +0500

    change in protected route

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -7,7 +7,7 @@ const ProtectedRoute = ({ path, component: Component }) => {
     const accessToken = Cookies.get('access_token');
     const currentUser = useSelector((state) => state.user.currentUser);
 
-    if (accessToken || currentUser) {
+    if (accessToken && currentUser) {
         if (path === '/sign-up' || path === '/sign-in') return <Navigate to='/' />
         else return <Component />
     }
